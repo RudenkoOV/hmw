d33c0350354d8378e3326f9ea11c54eea87ffd6e
RudenkoOV <kpaxx@ukr.net>
2023-05-21 11:55:41 +0300

    first C

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/hmw">
+        <BrowserRouter basename="/vite-deploy/">
           <App />
         </BrowserRouter>
       </PersistGate>
