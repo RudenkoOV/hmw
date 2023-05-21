880e5eddbf0f831e9bb5d1818df5223b9608f4d6
RudenkoOV <kpaxx@ukr.net>
2023-05-21 14:56:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { persistor, store } from './components/Redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './components/Redux/store';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      
         <BrowserRouter basename="/hmw/">
           <App />
         </BrowserRouter>
-      </PersistGate>
+  
     </Provider>
   </React.StrictMode>
 );
