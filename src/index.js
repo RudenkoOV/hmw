ac4abe80040e7acbe6e782af563b862cc933a130
RudenkoOV <kpaxx@ukr.net>
2023-05-21 14:51:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/hmw">
+        <BrowserRouter basename="/hmw/">
           <App />
         </BrowserRouter>
       </PersistGate>
